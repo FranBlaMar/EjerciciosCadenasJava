a34cbd5c381fd0ce1e3d0b6faabb771358f4f4db
Fran <franbl98@gmail.com>
2021-01-19 09:40:06 +0100

    Finalizado y comprobado primer metodo

--- EjerciciosString/src/EjerciciosCadenas.java
+++ EjerciciosString/src/EjerciciosCadenas.java
@@ -1,9 +1,19 @@
 
 public class EjerciciosCadenas {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main (String[] args) {
+		System.out.println(charactersInString("Hola, hoy tengo que hacer cosas", 'h'));
 	}
-
+	
+	public static int charactersInString (String cadena, char caracter) {
+		cadena = cadena.toUpperCase();
+		caracter= Character.toUpperCase(caracter);
+		int cont = 0;
+		for (int i= 0; i < cadena.length();i++) {
+			if (cadena.charAt(i) == caracter) {
+				cont ++;
+			}
+		}
+		return cont;
+	}
+	
 }
